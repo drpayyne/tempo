a57666acaf3f3ae2752bdaf80be5cd53d55eca75
Adarsh Manickam <adarsh.apple@icloud.com>
2021-04-06 22:06:25 +0530

    Initial Commit

---
+++ Tempo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tempo
+//
+//  Created by WX938YV on 06/04/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

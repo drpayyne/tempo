07fe8bc0fdea43571a876d0d60492899a80558a7
Adarsh Manickam <adarsh.apple@icloud.com>
2021-04-07 21:36:34 +0530

    Moved tempo logic to AppDelegate and added comments

--- Tempo/AppDelegate.swift
+++ Tempo/AppDelegate.swift
@@ -11,33 +11,64 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
+    var timer: Timer = Timer()
+    var tempo: Int = 0
+    var viewController: ViewController = ViewController()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Initialize status item
         statusItem.button?.title = "⏱"
         statusItem.button?.target = self
         statusItem.button?.action = #selector(showPopover)
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    @objc func showPopover() {
+        
+        // Get storyboard
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
-        guard let viewController = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
+        
+        // Get ViewController
+        guard let view = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
             fatalError("Unable to find ViewController!")
         }
         
+        // Set ViewController
+        viewController = view
+        
+        // Initialize timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Kill timer
+        timer.invalidate()
+    }
+
+    @objc func showPopover() {
+        // Get status item's button
         guard let button = statusItem.button else {
             fatalError("Couldn't find status item button!")
         }
         
+        // Initialize popover
         let popoverView = NSPopover()
         popoverView.contentViewController = viewController
         popoverView.behavior = .transient
         popoverView.show(relativeTo: button.bounds, of: button, preferredEdge: .maxY )
         popoverView.contentViewController?.view.window?.makeKey()
+    }
+    
+    @objc func timerCounter() -> Void {
+        // Increment tempo
+        tempo += 1
+        
+        // Set formatted tempo to popover's label
+        viewController.tempoLabel?.stringValue = makeTempoString(with: tempo)
+    }
+    
+    //   tempo / 3600       - h
+    //  (tempo % 3600) / 60 - m
+    //  (tempo % 3600) % 60 - s
+    func makeTempoString(with tempo: Int) -> String {
+        // Format tempo in "1h 1m"
+        return "\((tempo / 3600))h \((tempo % 3600) / 60)m"
     }
 }
 
--- Tempo/ViewController.swift
+++ Tempo/ViewController.swift
@@ -9,28 +9,9 @@ import Cocoa
 
 class ViewController: NSViewController {
     @IBOutlet var tempoLabel: NSTextField!
-
-    var timer: Timer = Timer()
-    var tempo: Int = 0
-    var inTempo: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tempoLabel.stringValue = "0m 0s"
-        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
-    }
-    
-    @objc func timerCounter() -> Void {
-        tempo += 1
-        tempoLabel.stringValue = makeTempoString(with: tempo)
-    }
-    
-    //  tempo / 3600        - h
-    //  (tempo % 3600) / 60 - m
-    //  (tempo % 3600) % 60 - s
-    func makeTempoString(with: Int) -> String {
-        return "\((tempo / 3600))h \((tempo % 3600) / 60)m"
     }
 
     override var representedObject: Any? {
@@ -39,6 +20,4 @@ class ViewController: NSViewController {
         }
     }
 
-
 }
-
